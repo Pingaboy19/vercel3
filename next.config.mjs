2372a3a3e34ba823e3aa29fd9afbaf8c9d598a16
Pingaboy19 <107768697+Pingaboy19@users.noreply.github.com>
2024-11-01 19:21:22 -0600

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
